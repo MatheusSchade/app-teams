eda6555826d95d9e67d3d3f9406b9ded0ca8caa6
Matheus Guilherme Schade <matheus_schade@hotmail.com>
2023-09-29 20:34:11 -0300

    feat: add statusBar estilizada e criado componente padrão de loading full screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
+import Loading from "@components/LoaderScreen";
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import Groups from '@screens/Groups';
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from './src/theme/index';
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle={`light-content`}
+        backgroundColor={`transparent`}
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
